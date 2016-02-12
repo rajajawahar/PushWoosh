2831223977c04b1f81e281d1832f266e58ccda8e
Raja Jawahar <rj.orgware@gmail.com>
2016-02-12 19:33:46 +0530

    initial comit for PushWoosh Implementation

---
+++ app/src/main/java/in/httporgware/pushwooshintegration/MainActivity.java
@@ -0,0 +1,132 @@
+package in.httporgware.pushwooshintegration;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.pushwoosh.BasePushMessageReceiver;
+import com.pushwoosh.BaseRegistrationReceiver;
+import com.pushwoosh.PushManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        registerReceivers();
+        PushManager pushManager = PushManager.getInstance(this);
+        try {
+            pushManager.onStartup(this);
+            pushManager.registerForPushNotifications();
+        } catch (Exception e) {
+        }
+
+        checkMessage(getIntent());
+
+    }
+
+    private void checkMessage(Intent intent) {
+        if (null != intent) {
+            if (intent.hasExtra(PushManager.PUSH_RECEIVE_EVENT)) {
+                showMessage("push message is " + intent.getExtras().getString(PushManager.PUSH_RECEIVE_EVENT));
+            } else if (intent.hasExtra(PushManager.REGISTER_EVENT)) {
+                showMessage("register");
+            } else if (intent.hasExtra(PushManager.UNREGISTER_EVENT)) {
+                showMessage("unregister");
+            } else if (intent.hasExtra(PushManager.REGISTER_ERROR_EVENT)) {
+                showMessage("register error");
+            } else if (intent.hasExtra(PushManager.UNREGISTER_ERROR_EVENT)) {
+                showMessage("unregister error");
+            }
+
+            resetIntentValues();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+
+        checkMessage(intent);
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void resetIntentValues() {
+        Intent mainAppIntent = getIntent();
+
+        if (mainAppIntent.hasExtra(PushManager.PUSH_RECEIVE_EVENT)) {
+            mainAppIntent.removeExtra(PushManager.PUSH_RECEIVE_EVENT);
+        } else if (mainAppIntent.hasExtra(PushManager.REGISTER_EVENT)) {
+            mainAppIntent.removeExtra(PushManager.REGISTER_EVENT);
+        } else if (mainAppIntent.hasExtra(PushManager.UNREGISTER_EVENT)) {
+            mainAppIntent.removeExtra(PushManager.UNREGISTER_EVENT);
+        } else if (mainAppIntent.hasExtra(PushManager.REGISTER_ERROR_EVENT)) {
+            mainAppIntent.removeExtra(PushManager.REGISTER_ERROR_EVENT);
+        } else if (mainAppIntent.hasExtra(PushManager.UNREGISTER_ERROR_EVENT)) {
+            mainAppIntent.removeExtra(PushManager.UNREGISTER_ERROR_EVENT);
+        }
+
+        setIntent(mainAppIntent);
+    }
+
+    BroadcastReceiver mBroadcastReceiver = new BaseRegistrationReceiver() {
+        @Override
+        public void onRegisterActionReceive(Context context, Intent intent) {
+            checkMessage(intent);
+        }
+    };
+
+    private BroadcastReceiver mReceiver = new BasePushMessageReceiver() {
+        @Override
+        protected void onMessageReceive(Intent intent) {
+            //JSON_DATA_KEY contains JSON payload of push notification.
+            showMessage("push message is " + intent.getExtras().getString(JSON_DATA_KEY));
+        }
+    };
+
+    public void registerReceivers() {
+        IntentFilter intentFilter = new IntentFilter(getPackageName() + ".action.PUSH_MESSAGE_RECEIVE");
+
+        registerReceiver(mReceiver, intentFilter, getPackageName() + ".permission.C2D_MESSAGE", null);
+
+        registerReceiver(mBroadcastReceiver, new IntentFilter(getPackageName() + "." + PushManager.REGISTER_BROAD_CAST_ACTION));
+    }
+
+    public void unregisterReceivers() {
+        //Unregister receivers on pause
+        try {
+            unregisterReceiver(mReceiver);
+        } catch (Exception e) {
+            // pass.
+        }
+
+        try {
+            unregisterReceiver(mBroadcastReceiver);
+        } catch (Exception e) {
+            //pass through
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceivers();
+    }
+}
